d66ac068633e3dc3f444e0480c873f2c6ac4281d
Stephen Melnick <stephenwmelnick@gmail.com>
2026-01-24 03:38:35 -0500

    Moved HealthComponent.h to the correct public folder

--- Source/PrimeJam/Private/Characters/Components/HealthComponent.cpp
+++ Source/PrimeJam/Private/Characters/Components/HealthComponent.cpp
@@ -1,7 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "HealthComponent.h"
+#include "Characters/Components/HealthComponent.h"
 
 
 UHealthComponent::UHealthComponent()
